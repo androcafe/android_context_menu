a0145fe686f1c71ccf178d9560308c42db81b077
Yayati09 <yaaytivs@gmail.com>
2018-07-30 14:31:29 +0530

    First commit

--- app/src/main/java/androcafe/com/myoptionmenuapplication/MainActivity.java
+++ app/src/main/java/androcafe/com/myoptionmenuapplication/MainActivity.java
@@ -3,6 +3,7 @@ package androcafe.com.myoptionmenuapplication;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
     Toolbar toolbar;
@@ -14,5 +15,12 @@ public class MainActivity extends AppCompatActivity {
 
         toolbar=findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
     }
 }
